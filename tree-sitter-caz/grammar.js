fdde1f76297ab3f7dccc078f10ab45bbf63d7bb2
viylouu <131220600+viylouu@users.noreply.github.com>
2025-08-23 16:00:49 -0600

    cdeez nuts

--- tree-sitter-caz/grammar.js
+++ tree-sitter-caz/grammar.js
@@ -10,8 +10,114 @@
 module.exports = grammar({
   name: "caz",
 
+  extras: $ => [
+    /\s/,
+    $.comment
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat(choice(
+      $._definition,
+      $._preproc
+    )),
+
+    _definition: $ => choice(
+      $.function_definition
+    ),
+
+    function_definition: $ => seq(
+      $.identifier,
+      '::',
+      $.parameter_list_defn,
+      '>>',
+      $._type,
+      $.scope
+    ),
+
+    parameter_list_defn: $ => seq(
+      '(',
+      optional(seq($.parameter_defn, repeat(seq(',', $.parameter_defn)))),
+      ')'
+    ),
+
+    parameter_defn: $ => seq(
+      $.identifier,
+      ':',
+      $._type
+    ),
+
+    parameter_list: $ => seq(
+      '(',
+      optional(seq($._expression, repeat(seq(',', $._expression)))),
+      ')'
+    ),
+
+    _type: $ => choice(
+      $.primitive_type,
+      $.slice_type
+      // todo: add more types
+    ),
+
+    primitive_type: $ => choice(
+      'raw',
+      'str'
+    ),
+
+    slice_type: $ => seq(
+      '[]',
+      $._type
+    ),
+
+    scope: $ => seq(
+      '{',
+      repeat($._statement),
+      '}'
+    ),
+
+    _statement: $ => choice(
+      $.return_statement,
+      $._preproc,
+      $.function_call
+    ),
+
+    return_statement: $ => seq(
+      'return',
+      $._expression
+    ),
+
+    function_call: $ => seq(
+      optional(repeat1(seq($.identifier, '::'))),
+      $.identifier,
+      $.parameter_list
+    ),
+
+    _expression: $ => choice(
+      $.identifier,
+      $.number,
+      $.string
+      // todo: more expressions
+    ),
+
+    string: $ => /"([^"\\]|\\.)*"/,
+
+    _preproc: $ => choice(
+      $.preproc_import
+    ),
+
+    preproc_import: $ => seq(
+      '#imp ',
+      '<',
+      seq($.identifier, repeat(seq('::', $.identifier))),
+      '>'
+    ),
+
+    identifier: $ => /[a-z]+/,
+    number: $ => /\d+/,
+
+    comment: $ => token(choice(
+      seq('//', /.*/),
+      seq('/*', repeat(choice(/[^*]/, /\*+[^*/]/)), '*/')
+    ))
   }
 });
+
