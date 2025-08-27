7740ca701d12b39753cc79f33f0038bfb1db96aa
viylouu <131220600+viylouu@users.noreply.github.com>
2025-08-26 22:46:31 -0600

    fastty

--- test.sh
+++ test.sh
@@ -1,14 +1,10 @@
 cd /media/storage/projects/langs/treesit-caz
 tree-sitter generate
-
-if [ $? -eq 1 ]; then
-    exit 1
-fi
-
+if [ $? -eq 1 ]; then exit 1; fi
 gitty "[$(date '+%m-%d-%Y %I:%M:%S %p')] AUTO UPDATE via ./test.sh"
 cd /home/viylouu/odin-tree-sitter
 rm -r /media/storage/projects/langs/caz/parsers/caz
-odin run build -- install-parser -name:caz -parser:https://github.com/viylouu/tree-sitter-caz #-path:/media/storage/projects/langs/caz_test/parsers/caz -yes
+odin run build -- install-parser -name:caz -parser:https://github.com/viylouu/tree-sitter-caz -yes #-path:/media/storage/projects/langs/caz_test/parsers/caz -yes
 mkdir /media/storage/projects/langs/caz/parsers/caz
 mv parsers/caz/parser.a /media/storage/projects/langs/caz/parsers/caz/
 mv parsers/caz/caz.odin /media/storage/projects/langs/caz/parsers/caz/
